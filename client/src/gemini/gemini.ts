fd7b91420cff007b1dec96d2f0208fc761efd8a3
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-08-03 22:06:20 +0200

    Update gemini.ts

--- client/src/gemini/gemini.ts
+++ client/src/gemini/gemini.ts
@@ -15,7 +15,7 @@ const safetySetting = [
   },
 ];
 
-const genAI = new GoogleGenerativeAI("AIzaSyBYWoqbUqwl4GgK2PACSPuWtUWxtNHX5iU");
+const genAI = new GoogleGenerativeAI("");
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
