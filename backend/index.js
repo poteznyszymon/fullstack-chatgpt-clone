dc7ed8e1144f56a3c15bc0dfbbeeca63f149f087
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-11-08 12:46:32 +0100

    Added port to env

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use("/api", authRoute);
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
   connectToMongo();
 });
