a668ee560477f61cca960e6a565ee89b41bf5f09
poteznyszymon <szymonfularczyk@o2.pl>
2025-02-12 12:40:31 +0100

    Update server configuration for production and development environments

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,15 @@ import messagesRoute from "./routes/messages.route.js";
 import { connectToMongo } from "./mongo/connectToDatabase.js";
 import cookieParser from "cookie-parser";
 
+import { fileURLToPath } from "url"
+import path from "path";
+
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
@@ -22,6 +27,16 @@ app.use(cookieParser());
 app.use("/api", authRoute);
 app.use("/api/chats", chatsRoute);
 app.use("/api/messages", messagesRoute);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
